6dd4c787fed38c22de0744408d5100e721b7c1a7
Benjamin Tomkins <bentomkins@hotmail.com>
2018-07-19 09:43:32 +0100

    added failing print statement feature test. statement initializer tested

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,3 +1,11 @@
 class Statement
 
+    # PUBLIC METHODS
+    attr_accessor :transactions
+
+
+    def initialize (account)
+        @transactions = account
+    end
+
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,23 +1,48 @@
+require 'date'
 require 'statement'
 
 describe Statement do
+
+    subject(:statement) { described_class.new }
+
 
     # date       || credit  ||  debit  || balance
     # 14/01/2012 ||         ||  500.00 || 2500.00
     # 13/01/2012 || 2000.00 ||         || 3000.00
     # 10/01/2012 || 1000.00 ||         || 1000.00
 
-    describe 'use case scenarios' do
+    @transaction_01 = {:date=> DateTime.parse("10/01/2012"), :credit=>1000, :debit=>nil, :balance=>1000}
+    @transaction_02 = {:date=> DateTime.parse("13/01/2012"), :credit=>2000, :debit=>nil, :balance=>3000}
+    @transaction_03 = {:date=> DateTime.parse("14/01/2012"), :credit=>nil,  :debit=>500, :balance=>2500}
 
-        xit 'prints a bank statement' do
+    @account_hash = [ @transaction_01,
+                      @transaction_02,
+                      @transaction_03 ]
+    
+
+    context 'use case scenarios' do
+
+        it 'prints a bank statement' do
+
+            expected_statement = "date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00 \n13/01/2012 || 2000.00 || || 3000.00 \n10/01/2012 || 1000.00 || || 1000.00 "
+
+            account = double('account')
+            allow(account).to receive(:to_hash).and_return(@account_hash)
+
+            statement = Statement.new(account)
+
+            expect { statement.printStatement }.to output(%Q(expected_statement)).to_stdout
         end
 
     end
 
 
-    describe 'unit tests' do
+    context 'unit tests' do
 
-        xit '.new' do
+        it '.new' do
+            account = double('account')
+            new_statement = Statement.new(account)
+            expect(new_statement.transactions).to eq(account)
         end
 
         xit '.printStatement' do
