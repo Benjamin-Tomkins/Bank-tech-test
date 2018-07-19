d92c16af67f102b656e9e2c7f3f2f7f5c43c4a85
Benjamin Tomkins <bentomkins@hotmail.com>
2018-07-19 11:05:40 +0100

    removed redundant unit test from statement spec

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,4 +1,5 @@
 require 'date'
+# require 'account'
 require 'statement'
 
 describe Statement do
@@ -8,38 +9,27 @@ describe Statement do
     # 13/01/2012 || 2000.00 ||         || 3000.00
     # 10/01/2012 || 1000.00 ||         || 1000.00
 
-    @transaction_01 = {:date=> DateTime.parse("10/01/2012"), :credit=>1000, :debit=>nil, :balance=>1000}
-    @transaction_02 = {:date=> DateTime.parse("13/01/2012"), :credit=>2000, :debit=>nil, :balance=>3000}
-    @transaction_03 = {:date=> DateTime.parse("14/01/2012"), :credit=>nil,  :debit=>500, :balance=>2500}
-
-    @account_hash = [ @transaction_01,
-                      @transaction_02,
-                      @transaction_03 ]
-
 
     describe 'use case scenarios' do
 
         it 'prints a bank statement' do
 
-            expected_statement = "date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00 \n13/01/2012 || 2000.00 || || 3000.00 \n10/01/2012 || 1000.00 || || 1000.00 "
+            transaction_01 = {:date=> DateTime.parse("10/01/2012"), :credit=>1000, :debit=>nil, :balance=>1000}
+            transaction_02 = {:date=> DateTime.parse("13/01/2012"), :credit=>2000, :debit=>nil, :balance=>3000}
+            transaction_03 = {:date=> DateTime.parse("14/01/2012"), :credit=>nil,  :debit=>500, :balance=>2500}
 
-            account = double('account')
-            allow(account).to receive(:to_hash).and_return(@account_hash)
+            account_hash = [transaction_01,transaction_02,transaction_03]
 
-            expect { Statement.printStatement }.to output(%Q(expected_statement)).to_stdout
+            expected_statement = "date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00 \n13/01/2012 || 2000.00 || || 3000.00 \n10/01/2012 || 1000.00 || || 1000.00"
+
+            account = double(Account, x: account_hash)
+            expect(account).to receive(:to_hash).and_return(account_hash)
+
+            statement = Statement.new(account)
+
+            expect(statement.printStatement).to eq("#{expected_statement}")
         end
 
     end
-
-
-    context 'unit tests' do
-
-        it '.printStatement' do
-
-        end
-
-    end
-
-
     
 end
